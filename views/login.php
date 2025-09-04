af9c78f94c950d8e85c0abff53f2786023d592aa
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2025-09-04 14:36:11 -0500

    Modificacion de diseño con boostrap

--- views/login.php
+++ views/login.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login de usuario</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+   <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 
-<body>
-    <div class="row">
-        <div class="col-md-12 d-flex justify-content-center align-items-center min-vh-100  bg-body-tertiary">
-            <form action="../controllers/login.php" method="post" class="bg-body-secondary p-5 rounded-5 shadow ">
+<body class="bg-login">
+    <div class="row bg-login">
+        <div class="col-md-12 d-flex justify-content-center align-items-center min-vh-100">
+            <form action="../controllers/login.php" method="post" class="bg-body-secondary border p-5 rounded-5 shadow ">
                 <div class="mb-3">
                     <h1 class="fw-bold text-center mb-4">Login de usuario</h1>
                     <label for="exampleInputEmail1" class="form-label">Documento de identidad</label>
@@ -21,7 +22,7 @@
                     <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                 </div>
-                <button type="submit" class="btn btn-primary w-100 mt-3 fw-bold">Ingresar</button>
+                <button type="submit" class="btn btn-primary w-100 mt-3 fs-5 fw-bold">Ingresar</button>
             </form>
         </div>
     </div>
